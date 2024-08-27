512fa4b71e901c8840dfec50c21a90b76ae86f4a
t1mofey <tmfztsv27@gmail.com>
2024-08-27 17:11:02 +0300

    Убрал скорость клика

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,7 +3,6 @@ let lastClickTime = 0;
 
 const cookie = document.getElementById('cookie');
 const counterElement = document.getElementById('clicker__counter');
-const speedElement = document.getElementById('clicker__speed');
 
 cookie.onclick = function() {
     // Увеличиваем счётчик кликов
@@ -16,12 +15,3 @@ cookie.onclick = function() {
     } else {
         cookie.width = 200;
     }
-
-    // Рассчитываем скорость клика
-    const currentTime = new Date().getTime();
-    if (lastClickTime !== 0) {
-        const clickSpeed = 1 / ((currentTime - lastClickTime) / 1000);
-        speedElement.textContent = clickSpeed.toFixed(2);
-    }
-    lastClickTime = currentTime;
-};
