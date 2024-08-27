01ea8ea838ec9fb0cfa0c880f9eee0f5d7f17a49
t1mofey <tmfztsv27@gmail.com>
2024-08-27 17:17:05 +0300

    Исправил всё

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,17 +1,15 @@
 let counter = 0;
-let lastClickTime = 0;
-
 const cookie = document.getElementById('cookie');
 const counterElement = document.getElementById('clicker__counter');
 
 cookie.onclick = function() {
-    // Увеличиваем счётчик кликов
     counter++;
     counterElement.textContent = counter;
-
-     // Меняем размер печеньки
-     if (cookie.width === 200) {
+    if (cookie.width === 200) {
         cookie.width = 250;
+        cookie.height = 250;
     } else {
         cookie.width = 200;
+        cookie.height = 200;
     }
+};
