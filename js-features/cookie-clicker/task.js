28441bc0ac71817365c89bad8aa2384f5d6ebb3b
t1mofey <tmfztsv27@gmail.com>
2024-08-28 15:00:51 +0300

    Упростил функцию onclick

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,13 +2,6 @@ let counter = 0;
 const cookie = document.getElementById('cookie');
 const counterElement = document.getElementById('clicker__counter');
 
-cookie.onclick = function() {
-    counter++;
-    counterElement.textContent = counter;
-    
-    if (cookie.style.transform === 'scale(1)') {
-        cookie.style.transform = 'scale(1.25)';
-    } else {
-        cookie.style.transform = 'scale(1)';
-    }
-};
+cookie.onclick = function () {
+    cookie.width = ++counterElement.textContent % 2 ? 250 : 200;
+}
